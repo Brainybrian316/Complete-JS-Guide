230cf141f712186f2f865206db39a2968ca42aeb
Brainybrian316 <brainybrian316@gmail.com>
2022-08-21 17:45:05 -0400

    actual strong attack. previous was regular attack

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -3,11 +3,18 @@ const ATTACK_VALUE = 10;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-	const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+	let maxDamage;
+	if (mode === 'ATTACK') {
+		maxDamage = ATTACK_VALUE;
+	} else if (mode === 'STRONG_ATTACK') {
+		maxDamage = STRONG_ATTACK_VALUE;
+	}
+	const damage = dealMonsterDamage(maxDamage);
 	currentMonsterHealth -= damage;
 	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
 	currentPlayerHealth -= playerDamage;
@@ -20,4 +27,13 @@ function attackHandler() {
 	}
 }
 
+function attackHandler() {
+	attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+	attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
