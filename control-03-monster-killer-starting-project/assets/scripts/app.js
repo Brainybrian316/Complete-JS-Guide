dc5a74e501e4009a880baf8494e8e71ea7d25e0b
Brainybrian316 <brainybrian316@gmail.com>
2022-08-22 17:35:00 -0400

    user chooses max life functionality

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,11 +1,19 @@
-let chosenMaxLife = 100;
 const ATTACK_VALUE = 10;
-let currentMonsterHealth = chosenMaxLife;
-let currentPlayerHealth = chosenMaxLife;
 const MONSTER_ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
 const HEAL_VALUE = 20;
+
+const enteredValue = prompt('Maximum life for you and the monster.', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+	chosenMaxLife = 100;
+}
+
 let hasBonusLife = true;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
