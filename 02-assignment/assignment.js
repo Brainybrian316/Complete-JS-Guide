78c6b263dfcd8101000c3b6dde82829217df7915
Brainybrian316 <brainybrian316@gmail.com>
2022-08-26 21:23:39 -0400

    loop testing

--- 02-assignment/assignment.js
+++ 02-assignment/assignment.js
@@ -1,11 +1,67 @@
 let result;
-const  userInput = 0;
+const userInput = 0;
 
-result = 18 + userInput; 
+result = 18 + userInput;
 
 result = result - 8;
 result = result * 5;
 result = result / 2;
 
 alert(result);
-alert(userInput)
+alert(userInput);
+
+// function q3() {
+// 	let sum = 0;
+// 	for (let i = 0; i < 5; i++) {
+// 		console.log(i);
+// 		sum = sum + i;
+// 		break;
+// 	}
+// 	console.log(sum); // ???
+// }
+
+// function q4() {
+// 	let sum = 0;
+// 	for (let i = 0; i < 5; i++) {
+// 		console.log('outer', i);
+// 		for (let j = 0; j < 2; j++) {
+// 			console.log('inner', j);
+// 			sum = sum + i + j;
+// 			break;
+// 		}
+// 	}
+// 	console.log(sum); // ???
+// }
+
+// function q5() {
+// 	let sum = 0;
+// 	for (let i = 0; i < 5; i++) {
+// 		console.log('outer', i);
+// 		for (let j = 0; j < 2; j++) {
+// 			console.log('inner', j);
+// 			sum = sum + i + j;
+// 			continue;
+// 		}
+// 	}
+// 	console.log(sum); // ???
+// }
+
+// function q6() {
+// 	let sum = 0;
+// 	for (let i = 0; i < 5; i++) {
+// 		console.log('outer', i);
+// 		for (let j = 0; j < 2; j++) {
+// 			console.log('inner', j);
+// 			if (i >= 2) {
+// 				continue;
+// 			}
+// 			sum = sum + i + j;
+// 		}
+// 	}
+// 	console.log(sum);
+// }
+
+// // q6();
+// // q5();
+// // q4();
+// // q3();
