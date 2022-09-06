897ca779cd52c5e1a70de0edf91a6948afd9d78f
Brainybrian316 <brainybrian316@gmail.com>
2022-09-06 18:35:37 -0400

    dom manipulation challenge 6

---
+++ 06-assignment/assignment.js
@@ -0,0 +1,13 @@
+const task1A = document.getElementById('task-1');
+task1A.style.backgroundColor = 'black';
+
+const task1B = document.querySelector('#task-1');
+task1B.style.color = 'white';
+
+const task2 = document.querySelector('head').querySelector('title');
+
+task2.textContent = 'Assignment - Solved!';
+
+const task3 = document.querySelector('h1');
+
+task3.textContent = 'Assignment - Solved!';
