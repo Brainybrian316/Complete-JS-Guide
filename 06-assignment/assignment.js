b506a51d4e5201474417a1ca1914665a85859970
Brainybrian316 <brainybrian316@gmail.com>
2022-09-06 18:42:17 -0400

    max's solution'

--- 06-assignment/assignment.js
+++ 06-assignment/assignment.js
@@ -11,3 +11,23 @@ task2.textContent = 'Assignment - Solved!';
 const task3 = document.querySelector('h1');
 
 task3.textContent = 'Assignment - Solved!';
+
+//  max solution
+
+const task1El1 = document.getElementById('task-1');
+const task1El2 = document.querySelector('li');
+
+task1A.style.color = 'white';
+
+task1El2.style.backgroundColor = 'black';
+
+const docTitle1 = document.querySelector('title');
+docTitle1.textContent = 'Assignment - Solved!';
+
+const docHead = document.head;
+const docTitle2 = document.head.querySelector('title');
+
+docTitle2.textContent = 'Assignment - Solved';
+
+const h1 = document.getElementsByTagName('h1');
+h1[0].textContent = 'Assignment - Solved!';
