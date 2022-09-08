28d5ed880714de4a91fd050061e387266d350c51
Brainybrian316 <brainybrian316@gmail.com>
2022-09-07 22:39:23 -0400

    traversing the dom

--- 06-assignment/assignment.js
+++ 06-assignment/assignment.js
@@ -1,16 +1,16 @@
-const task1A = document.getElementById('task-1');
-task1A.style.backgroundColor = 'black';
+// const task1A = document.getElementById('task-1');
+// task1A.style.backgroundColor = 'black';
 
-const task1B = document.querySelector('#task-1');
-task1B.style.color = 'white';
+// const task1B = document.querySelector('#task-1');
+// task1B.style.color = 'white';
 
-const task2 = document.querySelector('head').querySelector('title');
+// const task2 = document.querySelector('head').querySelector('title');
 
-task2.textContent = 'Assignment - Solved!';
+// task2.textContent = 'Assignment - Solved!';
 
-const task3 = document.querySelector('h1');
+// const task3 = document.querySelector('h1');
 
-task3.textContent = 'Assignment - Solved!';
+// task3.textContent = 'Assignment - Solved!';
 
 //  max solution
 
