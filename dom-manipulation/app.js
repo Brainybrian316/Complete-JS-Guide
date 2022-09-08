943b9e4ab36b116a85b2ccc12e2761b323989a89
Brainybrian316 <brainybrian316@gmail.com>
2022-09-07 22:44:21 -0400

    dom stylign

--- dom-manipulation/app.js
+++ dom-manipulation/app.js
@@ -1,20 +1,20 @@
-// // const listItemElements = document.querySelectorAll('li');
-// const listItemElements = document.getElementsByTagName('li'); //most commonly used
+// const listItemElements = document.querySelectorAll('li');
+const listItemElements = document.getElementsByTagName('li'); //most commonly used
 
-// for (const listItemEl of listItemElements) {
-// 	console.dir(listItemEl);
-// }
+for (const listItemEl of listItemElements) {
+	console.dir(listItemEl);
+}
 
-// const h1 = document.getElementById('main-title');
+const h1 = document.getElementById('main-title');
 
-// h1.textContent = 'Some new title!';
-// h1.style.color = 'white';
-// h1.style.backgroundColor = 'black';
+h1.textContent = 'Some new title!';
+h1.style.color = 'white';
+h1.style.backgroundColor = 'black';
 
-// const li = document.querySelector('li:last-of-type');
-// li.textContent = li.textContent + ' (Changed!)';
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent + ' (Changed!)';
 
-// const body = document.body;
+const body = document.body;
 
 // const ul = document.body.firstElementChild.nextElementSibling;
 // const firstLi = ul.firstElementChild;
