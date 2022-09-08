5d9a296e96351414da8ea77afd5d23a75ec564f9
Brainybrian316 <brainybrian316@gmail.com>
2022-09-07 22:55:52 -0400

    classlist className

--- dom-manipulation/app.js
+++ dom-manipulation/app.js
@@ -16,7 +16,29 @@ li.textContent = li.textContent + ' (Changed!)';
 
 const body = document.body;
 
+// *dom traversal
 // const ul = document.body.firstElementChild.nextElementSibling;
 // const firstLi = ul.firstElementChild;
-
 // console.log(firstLi);
+
+// *dom Styling
+
+const section = document.querySelector('section');
+// section.style.backgroundColor = 'blue';
+section.className = 'red-bg';
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+	/* className version
+	// if (section.className === 'red-bg visible') {
+	// 	section.className = 'red-bg invisible';
+	// } else {
+	// 	section.className = 'red-bg visible';
+	// }
+	// ternary operator
+	// section.className =
+	// 	section.className === 'red-bg visible' ? 'red-bg invisible' : 'red-bg visible';
+	// */
+	section.classList.toggle('invisible');
+});
