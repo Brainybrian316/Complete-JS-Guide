87c2e8ee581c84c33029e34f9b0e08a03d811ea6
Brainybrian316 <brainybrian316@gmail.com>
2022-09-10 11:37:02 -0400

    add moving checks and inputs

--- Movie-Project/assets/scripts/app.js
+++ Movie-Project/assets/scripts/app.js
@@ -7,6 +7,10 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
+// alternative to above
+//  const userInputs = addMovieModal.getElementsByTagName('input');
 
 const toggleMovieModal = () => {
 	addMovieModal.classList.toggle('visible');
@@ -17,8 +21,25 @@ const toggleBackdrop = () => {
 	backdrop.classList.toggle('visible');
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
 	toggleMovieModal;
+};
+
+const addMovieHandler = () => {
+	const titleValue = userInputs[0].value;
+	const imageUrlValue = userInputs[1].value;
+	const ratingValue = userInputs[2].value;
+
+	if (
+		titleValue.trim() === '' ||
+		imageUrlValue.trim() === '' ||
+		ratingValue.trim() === '' ||
+		+ratingValue < 1 || // alternative to writing parseInt
+		+ratingValue > 5
+	) {
+		alert('Please enter valid values. (rating between 1 and 5). ');
+		return;
+	}
 };
 
 const backdropClickHandler = () => {
@@ -29,4 +50,6 @@ startAddMovieButton.addEventListener('click', toggleMovieModal);
 
 backdrop.addEventListener('click', backdropClickHandler);
 
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
