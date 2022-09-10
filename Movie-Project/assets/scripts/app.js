7af19195acdade60f77178d31f907487cf31a644
Brainybrian316 <brainybrian316@gmail.com>
2022-09-10 10:16:11 -0400

    alternatives for dom

--- Movie-Project/assets/scripts/app.js
+++ Movie-Project/assets/scripts/app.js
@@ -1,2 +1,7 @@
+// * alternatives to selecting dom elements:
+// const addMovieModal = document.querySelector('add-modal');
+// const addMovieModal = document.body.children[1];
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
+
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
