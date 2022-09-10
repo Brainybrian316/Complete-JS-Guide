70c4c9baa5629759b35bcbd4f0a55121173dd0c5
Brainybrian316 <brainybrian316@gmail.com>
2022-09-10 10:30:28 -0400

    cancel button functional

--- Movie-Project/assets/scripts/app.js
+++ Movie-Project/assets/scripts/app.js
@@ -6,6 +6,7 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
 const toggleMovieModal = () => {
 	addMovieModal.classList.toggle('visible');
@@ -16,6 +17,10 @@ const toggleBackdrop = () => {
 	backdrop.classList.toggle('visible');
 };
 
+const cancelAddMovie = () => {
+	toggleMovieModal;
+};
+
 const backdropClickHandler = () => {
 	toggleMovieModal();
 };
@@ -23,3 +28,5 @@ const backdropClickHandler = () => {
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 
 backdrop.addEventListener('click', backdropClickHandler);
+
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
