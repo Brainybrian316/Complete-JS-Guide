7ae8f58ba5afabbc7f9a1403721bfddc96057288
Brainybrian316 <brainybrian316@gmail.com>
2022-09-10 10:27:26 -0400

    backdrop display and remove backdrop when clicking out of modal

--- Movie-Project/assets/scripts/app.js
+++ Movie-Project/assets/scripts/app.js
@@ -5,9 +5,21 @@
 
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
 
 const toggleMovieModal = () => {
 	addMovieModal.classList.toggle('visible');
+	toggleBackdrop();
+};
+
+const toggleBackdrop = () => {
+	backdrop.classList.toggle('visible');
+};
+
+const backdropClickHandler = () => {
+	toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+
+backdrop.addEventListener('click', backdropClickHandler);
