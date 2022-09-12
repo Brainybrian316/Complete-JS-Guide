6f0edda34dbf0aa24fe935d08b8566387c0ee99d
Brainybrian316 <brainybrian316@gmail.com>
2022-09-12 19:45:34 -0400

    maps

--- arrays-iterables/maps.js
+++ arrays-iterables/maps.js
@@ -1,9 +1,37 @@
-const ids = new Set(['hi', 'from', 'set']);
-ids.add(2);
-if (ids.has('hi')) {
-	ids.delete('hi');
+// const ids = new Set(['hi', 'from', 'set']);
+// ids.add(2);
+// if (ids.has('hi')) {
+// 	ids.delete('hi');
+// }
+// for (const entry of ids.entries()) {
+// 	console.log(entry[0]);
+// }
+// console.log(ids);
+
+// * maps
+const person1 = { name: 'max' };
+const person2 = { name: 'manuel' };
+
+const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+
+personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const entry of personData.entries()) {
+	console.log(entry);
 }
-for (const entry of ids.entries()) {
-	console.log(entry[0]);
+// array destructuring method of for loop
+for (const [key, value] of personData.entries()) {
+	console.log(key, value);
 }
-console.log(ids);
+
+// good for getting the keys only
+for (const key of personData.keys()) {
+	console.log(key);
+}
+
+// good for getting the values only
+for (const values of personData.values()) {
+	console.log(values);
+}
