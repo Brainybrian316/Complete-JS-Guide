d235f538d22a8cd32bd3a8046720de227dee1deb
Brainybrian316 <brainybrian316@gmail.com>
2022-09-12 21:34:49 -0400

    task 1

---
+++ 07-assignment/assignment.js
@@ -0,0 +1,20 @@
+// ! Task 1
+// const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// const filteredNumbers = numbers.filter((num) => {
+// 	return num > 5;
+// });
+
+// console.log(filteredNumbers);
+
+// const mapNumbers = numbers.map((number) => {
+// 	return { key: number };
+// });
+
+// console.log(mapNumbers);
+
+// const reduceNumbers = numbers.reduce((prevValue, currValue) => {
+// 	return prevValue * currValue;
+// });
+
+// console.log(reduceNumbers);
