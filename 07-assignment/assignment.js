b5fcf4408dcfa03231ba0d2362c8b923d885afa9
Brainybrian316 <brainybrian316@gmail.com>
2022-09-12 23:24:43 -0400

    assignment done

--- 07-assignment/assignment.js
+++ 07-assignment/assignment.js
@@ -1,20 +1,36 @@
 // ! Task 1
-// const numbers = [1, 2, 3, 4, 5, 6, 7];
+const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-// const filteredNumbers = numbers.filter((num) => {
-// 	return num > 5;
-// });
+const filteredNumbers = numbers.filter((num) => {
+	return num > 5;
+});
 
-// console.log(filteredNumbers);
+console.log(filteredNumbers);
 
-// const mapNumbers = numbers.map((number) => {
-// 	return { key: number };
-// });
+const mapNumbers = numbers.map((number) => {
+	return { key: number };
+});
 
-// console.log(mapNumbers);
+console.log(mapNumbers);
 
-// const reduceNumbers = numbers.reduce((prevValue, currValue) => {
-// 	return prevValue * currValue;
-// });
+const reduceNumbers = numbers.reduce((prevValue, currValue) => {
+	return prevValue * currValue;
+});
 
-// console.log(reduceNumbers);
+console.log(reduceNumbers);
+
+const MaxValue = (...values) => {
+	const bigValue = Math.max(...values);
+	const smallValue = Math.min(...values);
+	return [bigValue, smallValue];
+};
+
+const [max, min] = MaxValue(...numbers);
+
+console.log(max, min);
+
+const setOfNumbers = new Set();
+setOfNumbers.add(2);
+setOfNumbers.add(5);
+
+console.log(setOfNumbers);
