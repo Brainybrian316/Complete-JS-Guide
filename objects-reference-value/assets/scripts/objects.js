31bb883c69e5cbeba8cfe7bb0cf1522936349342
Brainybrian316 <brainybrian316@gmail.com>
2023-01-07 13:45:43 -0500

    236 getter and setters

--- objects-reference-value/assets/scripts/objects.js
+++ objects-reference-value/assets/scripts/objects.js
@@ -24,15 +24,15 @@ const renderMovies = (filter = '') => {
     const { info, ...otherProperties } = movie;
     console.log(otherProperties);
     // *destructure with new name
-    const { title: movieTitle } = info;
+    // const { title: movieTitle } = info;
     // ^destructure a function with bind for future execution
     let { getFormattedTitle } = movie;
     // getFormattedTitle = getFormattedTitle.bind(movie);
     // @alternative using call method  to execute func right away
-    let text = getFormattedTitle().call(movie) + ' - ';
+    let text = getFormattedTitle.call(movie) + ' - ';
     // *dynamic user property
     for (const key in info) {
-      if (key !== 'title') {
+      if (key !== 'title' && key !== '_title') {
         text = text + `${key}: ${info[key]}`;
       }
     }
@@ -46,14 +46,24 @@ const addMovieHandler = () => {
   const extraName = document.getElementById('extra-name').value;
   const extraValue = document.getElementById('extra-value').value;
 
-  if (title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+  if (extraName.trim() === '' || extraValue.trim() === '') {
     return;
   }
 
   const newMovie = {
     info: {
-      // if key name and value name are the same you can use this syntax
-      title,
+      set title(val) {
+        if (val.trim() === '') {
+          this._title = 'DEFAULT';
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
+      // // if key name and value name are the same you can use this syntax
+      // title: title,
       // we don't know extra name from user so we this nifty syntax
       [extraName]: extraValue,
     },
@@ -62,6 +72,9 @@ const addMovieHandler = () => {
       return this.info.title.toUpperCase();
     },
   };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
 
   movies.push(newMovie);
   renderMovies();
